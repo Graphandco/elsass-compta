3d9582a2b11584fd31d680c30aa3a5311bfdd0df
Régis Daum <rdaum@john-steel.com>
2025-06-10 12:40:55 +0200

    Fix BG valeurs

--- frontend/components/homepage/Valeurs.jsx
+++ frontend/components/homepage/Valeurs.jsx
@@ -18,38 +18,40 @@ export default function Valeurs({ valeurs }) {
    // const yButton = useTransform(scrollYProgress, [0, 1], ["0%", "-50%"]);
 
    return (
-      <section className="wrapper py-20 bg-primary-light">
-         <FadeInOnView>
-            <h2 className="text-primary font-normal text-3xl sm:text-4xl md:text-5xl relative z-10">
-               Nos valeurs
-            </h2>
-         </FadeInOnView>
+      <section className="bg-primary-light">
+         <div className="wrapper py-20">
+            <FadeInOnView>
+               <h2 className="text-primary font-normal text-3xl sm:text-4xl md:text-5xl relative z-10">
+                  Nos valeurs
+               </h2>
+            </FadeInOnView>
 
-         <FadeInOnView className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 mt-14">
-            {valeurs.map((valeur) => (
-               <div
-                  key={valeur.id}
-                  className="h-full bg-white p-3 rounded-lg shadow-md"
-               >
-                  <Image
-                     src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${valeur?.image?.url}`}
-                     alt={valeur.title}
-                     width={valeur.image.width}
-                     height={valeur.image.height}
-                     className="rounded-lg"
-                  />
-                  <h3 className="text-primary font-normal text-lg md:text-xl mt-3 mb-1">
-                     {valeur.title}
-                  </h3>
-                  <p className="text-sm">{valeur.description}</p>
-               </div>
-            ))}
-         </FadeInOnView>
-         <FadeInOnView className="text-center mt-10">
-            <Button asChild>
-               <Link href="/cabinet">Découvrez notre équipe</Link>
-            </Button>
-         </FadeInOnView>
+            <FadeInOnView className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 mt-14">
+               {valeurs.map((valeur) => (
+                  <div
+                     key={valeur.id}
+                     className="h-full bg-white p-3 rounded-lg shadow-md"
+                  >
+                     <Image
+                        src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${valeur?.image?.url}`}
+                        alt={valeur.title}
+                        width={valeur.image.width}
+                        height={valeur.image.height}
+                        className="rounded-lg"
+                     />
+                     <h3 className="text-primary font-normal text-lg md:text-xl mt-3 mb-1">
+                        {valeur.title}
+                     </h3>
+                     <p className="text-sm">{valeur.description}</p>
+                  </div>
+               ))}
+            </FadeInOnView>
+            <FadeInOnView className="text-center mt-10">
+               <Button asChild>
+                  <Link href="/cabinet">Découvrez notre équipe</Link>
+               </Button>
+            </FadeInOnView>
+         </div>
       </section>
    );
 }
