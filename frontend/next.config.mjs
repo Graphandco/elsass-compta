682c27ed2e5e890739062caede57756a9f712f15
Régis Daum <rdaum@john-steel.com>
2025-09-22 14:48:29 +0200

    Add sitemap

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    images: {
-      domains: ["localhost", "admin.elsass-compta.fr"],
+      domains: ["localhost", "admin-elsass-compta.graphandco.net"],
       remotePatterns: [
          {
             protocol: "https",
-            hostname: "admin.elsass-compta.fr",
+            hostname: "admin-elsass-compta.graphandco.net",
             pathname: "/uploads/**",
          },
       ],
